418d8adb0305f490791b2581362262fc6ea50c3e
Babasulaiman <habeebbabasulaiman@gmail.com>
2023-03-18 02:00:12 -0700

    updated

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
---1-create_database_if_missing.sql
-CREATE DATABASE IF NOT EXISTS 'hbtn_0c_0';
+-- A script that creates the database hbtn_0c_0 in MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
